fc0e74173c1393c93ea6603a420e3eb949f4c203
SaydAliUsmanov <usmanov.guno@mail.ru>
2021-11-30 13:42:05 +0300

    Added auth

--- models/Product.model.js
+++ models/Product.model.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
-const productSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
   },
-  price: {
-    type: Number,
-    required: true
-  }
-},
   { timestamps: true }
 );
 
